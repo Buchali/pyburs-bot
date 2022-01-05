4f89097a2af95570fd51b24b11d828446cbcd0b8
amir <amir@Mac.local>
2022-01-05 17:01:10 +0330

    add delete symbols from portfolio option

--- src/constants.py
+++ src/constants.py
@@ -4,43 +4,62 @@ from src.utils.keyboard import create_keyboard
 
 keys = SimpleNamespace(
     settings = ':gear: تنظیمات',
-    add_stock =  ':plus: افزودن نماد به پرتفو',
+    add_symbol =  ':inbox_tray: افزودن نماد به پرتفو',
     portfolio = ':basket: پرتفو',
     exit = ':cross_mark: خروج',
+    delete_symbol = ':wastebasket: حذف نماد'
 )
 
 keyboards = SimpleNamespace(
     main = create_keyboard(keys.portfolio),
     exit = create_keyboard(keys.exit),
-    stock = create_keyboard(keys.add_stock, keys.exit, row_width=1),
+    symbol = create_keyboard(keys.add_symbol, keys.exit, row_width=1),
+    portfolio = create_keyboard(keys.delete_symbol, keys.exit, row_width=1),
 )
 
 states  = SimpleNamespace(
-    main = 'MAIN',
-    stock = 'STOCK',
+    MAIN = 'MAIN',
+    ADDING_SYMBOL = 'ADDING SYMBOL',
+    DELETE = 'DELETE',
+    PORTFOLIO = 'PORTFOLIO',
 )
 
 START_MESSAGE = (
     'سلام <strong>{first_name}</strong>، خوش اومدی :man_raising_hand:'
-    '\n\n  نماد بورسی مورد نظرتو تایپ کن:'
+    f"\n {70 * 'ـ'}"
+    '\n  :red_triangle_pointed_down: این ربات برای یادآوری حدسود و حدضرر در بازار بورس تهران طراحی شده. :warning:'
+    f"\n {70 * 'ـ'}"
+    '\n\n  نماد بورسی مورد نظرتو تایپ کن. :speech_balloon:'
 )
 EXIT_MESSAGE = (
     'خارج شدی.'
     f"\n {30 * 'ـ'}"
-    '\n\n اگه می‌خوای نماد جدید تایپ کن.'
+    '\n\n اگه می‌خوای نماد جدید تایپ کن. :speech_balloon:'
 )
-NOT_STOCK_MESSAGE = (
+NOT_SYMBOL_MESSAGE = (
     'متاسفانه، نمادی با نام {symbol} یافت نشد! :thumbs_down:'
     f"\n {70 * 'ـ'}"
-    '\n\n اگه می‌خوای نماد جدید تایپ کن.'
+    '\n\n اگه می‌خوای نماد جدید تایپ کن. :speech_balloon:'
 )
-ADD_STOCK_MESSAGE = (
+ADD_SYMBOL_MESSAGE = (
     '<strong>{symbol}</strong> با موفقیت به پورتفو اضافه شد :check_mark_button:'
     f"\n {70 * 'ـ'}"
-    '\n\n اگه می‌خوای نماد جدید تایپ کن.'
+    '\n\n اگه می‌خوای نماد جدید تایپ کن. :speech_balloon:'
 )
 PORTFOLIO_MESSAGE = (
-    f"پورتفوی شما :basket: \n"
+    "پورتفوی شما :basket: \n"
     f"{20 * '_'} \n\n"
     '{portfolio}'
+)
+DELETE_SYMBOL_MESSAGE = (
+    "نمادی که می‌خوای حذف شه رو تایپ کن."
+)
+DELETED_MESSAGE = (
+    '<strong>{symbol}</strong> با موفقیت از پورتفو حذف شد :basket::check_mark_button:'
+)
+NOT_IN_PORTFOLIO_MESSAGE = (
+    'متاسفانه، نمادی با نام {symbol} در پرتفو شما وجود ندارد! :thumbs_down:'
+)
+EMPTY_PORTFOLIO_MESSAGE = (
+    "هیچ نمادی در پرتفو وجود ندارد! "
 )
--- src/run.py
+++ src/run.py
@@ -3,7 +3,7 @@ from loguru import logger
 
 import constants
 from src.bot import bot
-from src.constants import keyboards, keys
+from src.constants import keyboards, keys, states
 from src.data import DATA_DIR
 from src.db import mongodb
 from src.users import Users
@@ -34,24 +34,48 @@ class BursBot():
 
         @self.bot.message_handler(commands=['start'])
         def start(message):
+            self.user.update_state(states.MAIN)
             self.send_message(
                 message.chat.id,
                 constants.START_MESSAGE.format(first_name=message.chat.first_name),
                 reply_markup=keyboards.main
                 )
+        @self.bot.message_handler(func=lambda message: message.text in self.user.portfolio)
+        def portfolio_symbol(message):
+            self.user.update_current_symbol(message.text)
+            # user wants to delete a symbol
+            if self.user.state == states.DELETE:
+                portfolio = self.user.portfolio
+                deleted_symbol = portfolio.pop(message.text, None)
+                if deleted_symbol is None:
+                    self.send_message(
+                    message.chat.id,
+                    constants.NOT_IN_PORTFOLIO_MESSAGE.format(symbol=message.text),
+                    reply_markup=keyboards.portfolio
+                    )
+                    self.user.update_state(states.PORTFOLIO)
+                else:
+                    self.user.update_portfolio(portfolio)
+                    self.send_message(
+                    message.chat.id,
+                    constants.DELETED_MESSAGE.format(symbol=message.text),
+                    reply_markup=keyboards.main
+                    )
+                    self.user.update_state(states.MAIN)
 
         @self.bot.message_handler(func=lambda message: message.text in self.stock_symbols)
-        def stock(message):
+        def symbol(message):
             self.user.update_current_symbol(message.text)
+
             # TODO: show basic information of the symbol to user
             self.send_message(
                 message.chat.id,
                 f"<strong>{self.stock_symbols[message.text]['name']}</strong>",
-                reply_markup=keyboards.stock
+                reply_markup=keyboards.symbol
             )
 
-        @self.bot.message_handler(regexp=keys.add_stock)
-        def add_stock(message):
+        @self.bot.message_handler(regexp=keys.add_symbol)
+        def add_symbol(message):
             current_symbol = self.user.current_symbol
             portfolio = self.user.portfolio
             portfolio[current_symbol] = self.stock_symbols[current_symbol]
@@ -59,19 +83,37 @@ class BursBot():
 
             self.send_message(
                 message.chat.id,
-                constants.ADD_STOCK_MESSAGE.format(symbol=current_symbol),
+                constants.ADD_SYMBOL_MESSAGE.format(symbol=current_symbol),
                 reply_markup=keyboards.main
                 )
+            self.user.update_state(states.MAIN)
 
         @self.bot.message_handler(regexp=keys.portfolio)
         def portfolio(message):
-            portfolio_text = '\n'.join(self.user.portfolio.keys())
+            if not self.user.portfolio.keys():
+                self.send_message(
+                message.chat.id,
+                constants.EMPTY_PORTFOLIO_MESSAGE,
+                reply_markup=keyboards.exit
+                )
+                return
 
+            portfolio_text = '\n'.join(self.user.portfolio.keys())
             self.send_message(
                 message.chat.id,
                 constants.PORTFOLIO_MESSAGE.format(portfolio=portfolio_text),
+                reply_markup=keyboards.portfolio
+                )
+            self.user.update_state(states.PORTFOLIO)
+
+        @self.bot.message_handler(regexp=keys.delete_symbol)
+        def delete_symbol(message):
+            self.send_message(
+                message.chat.id,
+                constants.DELETE_SYMBOL_MESSAGE,
                 reply_markup=keyboards.exit
                 )
+            self.user.update_state(states.DELETE)
 
         @self.bot.message_handler(regexp=keys.exit)
         def exit(message):
@@ -80,10 +122,11 @@ class BursBot():
                 constants.EXIT_MESSAGE,
                 reply_markup=keyboards.main
                 )
+            self.user.update_state(states.MAIN)
 
         @self.bot.message_handler(func=lambda m: True)
         def echo(message):
-            self.send_message(message.chat.id, constants.NOT_STOCK_MESSAGE.format(symbol=message.text))
+            self.send_message(message.chat.id, constants.NOT_SYMBOL_MESSAGE.format(symbol=message.text))
 
     def run(self):
         logger.info('Bot is running...')
